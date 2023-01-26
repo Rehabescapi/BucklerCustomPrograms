d94553525a19e6dfba47bfa36c0c47553386cda8
Jason Lehmann <Jasonmlehmann@gmail.com>
2023-01-26 09:46:25 -0800

    Working Struct

--- software/apps/gpio_blank/gpio.h
+++ software/apps/gpio_blank/gpio.h
@@ -2,13 +2,48 @@
 
 #include "nrf.h"
 #include "stdbool.h"
-struct gpio
+typedef struct 
 {
-    uint32_t var_OUTSET;
-    uint32_t var_IN;
-    uint32_t var_OUTCLEAR; 
+    uint32_t OUTSET;
+    uint32_t OUTCLEAR;
+    uint32_t IN; 
     uint32_t arr[32];
-};
+} gpio;
+
+typedef struct {
+    uint32_t PIN0;
+    uint32_t PIN1;
+    uint32_t PIN2;
+    uint32_t PIN3;
+    uint32_t PIN4;
+    uint32_t PIN5;
+    uint32_t PIN6;
+    uint32_t PIN7;
+    uint32_t PIN8;
+    uint32_t PIN9;
+    uint32_t PIN10;
+    uint32_t PIN11;
+    uint32_t PIN12;
+    uint32_t PIN13;
+    uint32_t PIN14;
+    uint32_t PIN15;
+    uint32_t PIN16;
+    uint32_t PIN17;
+    uint32_t PIN18;
+    uint32_t PIN19;
+    uint32_t PIN20;
+    uint32_t PIN21;
+    uint32_t SWITCH0;
+    uint32_t BLUE;
+    uint32_t YLLO;
+    uint32_t RED;
+    uint32_t PIN26;
+    uint32_t PIN27;
+    uint32_t BUTTON0;
+    uint32_t PIN29;
+    uint32_t PIN30;
+    uint32_t PIN31;
+} pins;
 
 typedef enum {
     INPUT = 0,
--- software/apps/gpio_blank/main.c
+++ software/apps/gpio_blank/main.c
@@ -30,16 +30,6 @@ static uint32_t* CNF0 = (uint32_t*) 0x5000070C;
 static uint32_t* IN = (uint32_t*) 0x50000510;
 static uint32_t* DIR = (uint32_t*) 0x5000514;
 
-static uint32_t* PIN0 = (uint32_t* ) 0x50000700;
-
-static uint32_t*  PIN23 = (uint32_t*) 0x5000075C;
-static uint32_t*  PIN24 = (uint32_t*) 0x50000760;
-static uint32_t*  PIN25 = (uint32_t*) 0x50000764;
-
-
-static uint32_t* PIN22 = (uint32_t*) 0x50000758; // Switch0 
-static uint32_t* PIN28 = (uint32_t*) 0x50000770;
-
 
 
 //static uint32_t* Button0
@@ -71,51 +61,35 @@ int main(void) {
   
 
   //Adding Light struct
-  struct gpio *hardware;
-
-  //hardware = (struct gpio*)malloc(sizeof(struct gpio));
+  
+  volatile pins* const pinD = (pins*) 0x50000700;
+  volatile gpio* const GPIO = (gpio*) 0x50000508;
  
-  hardware->var_OUTSET = OUTSET;
-  hardware->var_OUTCLEAR = OUTCLEAR;
-  hardware->var_IN = IN;
-  hardware->arr[0]= PIN0;//0x50000700
-  for(int i =1;  i< 32; i++)
-  {
-    uint32_t *x = PIN0 + sizeof(uint8_t)*i;
-
-    //printf("%p \n\n", x);
-    hardware->arr[i] = (uint32_t*) x;
-    printf("%p \n\n",hardware->arr[i]);
-  }
   
+  //hardwareptr->arr= &PIN0;//0x50000700
   
+ pinD->BLUE = 1;
+  
+  pinD->YLLO = 1; // Yellow
+  pinD->RED =1; // Red
 
+  pinD->SWITCH0 = 0;
+  pinD->SWITCH0 = 1 <<1;
+  pinD->BUTTON0 =0;
+  pinD->BUTTON0 =1 <<1;
 
-
-  printf("%p for initial point of arr at first element\n\n", hardware->arr[0]);
-  //printf("%p  should end in 758 if you went to arr 22.\n\n" ,hardware->arr[22]);
-
-  hardware->arr[23] =1;// Blue *PIN23
-  //*PIN24 = 1; // Yellow
-  *PIN25 =1; // Red
-
-  *PIN22 = 0;
-  *PIN22 = 1 <<1;
-  *PIN28 =0;
-  *PIN28 =1 <<1;
-
-  *OUTCLEAR = 1 <<28;
-  *OUTCLEAR = 1 <<22;
+  GPIO->OUTCLEAR = 1 <<28;//clear
+  GPIO->OUTCLEAR = 1 <<22;//clear
 
 
   //Setting Pins as inputs
   *IN = 1 <<28;
   *IN = 1 <<22;
 
-  *OUTCLEAR =  1 << 25; 
+  GPIO->OUTCLEAR =  1 << 25; //clear
   printf("Turning on 25.");
   nrf_delay_ms(3000);
-  *OUTSET = 1 << 25;
+  GPIO->OUTSET = 1 << 25;
   printf("Turning off Blue\n");
 
 
@@ -131,8 +105,8 @@ int main(void) {
     {
       //printf("toggle back on\n");
       temp = !temp;
-      *OUTCLEAR = 1 <<23;
-      *OUTSET = 1 <<25;
+      GPIO->OUTCLEAR = 1 <<23;
+      GPIO->OUTSET = 1 <<25;
 
       
       
@@ -142,8 +116,8 @@ int main(void) {
     {
       temp = true;
       
-      *OUTSET =  1 << 23; 
-      *OUTCLEAR =1 <<25;
+      GPIO->OUTSET =  1 << 23; 
+      GPIO->OUTCLEAR =1 <<25;
     }
 
 

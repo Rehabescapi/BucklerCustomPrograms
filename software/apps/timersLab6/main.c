ab94b436d738dcc78ae5789afc07d33b8756996e
Jason Lehmann <Jasonmlehmann@gmail.com>
2023-02-22 09:55:50 -0800

    inserting intterupts issues

--- software/apps/timersLab6/main.c
+++ software/apps/timersLab6/main.c
@@ -22,6 +22,22 @@
 #include "nrf_serial.h"
 
 #include "buckler.h"
+
+
+
+void GPIOTE_IRQHandler(void) {//Already written in part 
+    NRF_GPIOTE->EVENTS_IN[0] = 0;
+    printf("Butttton interrupt received\n\n");
+    //nrf_delay_ms(100);
+    gpio_clear(25);
+    nrf_delay_ms(2000);
+    gpio_set(25);
+
+
+    
+    
+}
+
 static int threeSecondCount = 93750;
 static int tX = 1, tY = 2; 
 void timer_init(){
@@ -40,7 +56,7 @@ void timer_init(){
   NRF_TIMER4->TASKS_START = 0x01;
 
 
-  //NRF_TIMER3 ->PRESCALER =0x09;
+  
   NRF_TIMER3 -> BITMODE = 0x02;
   NRF_TIMER3->MODE = 0x01;
   NRF_TIMER3->TASKS_START = 0x01;
@@ -85,7 +101,9 @@ int main(void) {
   nrf_gpio_pin_dir_set(BUCKLER_LED1, NRF_GPIO_PIN_DIR_OUTPUT);
   nrf_gpio_pin_dir_set(BUCKLER_LED2, NRF_GPIO_PIN_DIR_OUTPUT);
 
-  timer_init();  
+  timer_init(); 
+  setDevices(); 
+  LABHandler();
 
 
   nrf_delay_ms(2000);
@@ -97,7 +115,7 @@ int main(void) {
   
   // loop forever
   while (1) {
-    if(NRF_TIMER4->EVENTS_COMPARE[tY]){
+    /*if(NRF_TIMER4->EVENTS_COMPARE[tY]){
       NRF_TIMER4->EVENTS_COMPARE[tY]=0;
       NRF_TIMER4->TASKS_CLEAR = 0x01;
       NRF_TIMER4->TASKS_START = 0x01;
@@ -106,6 +124,10 @@ int main(void) {
       NRF_TIMER3->TASKS_COUNT = 0x01;
       printf("3 Seconds have happened\n");
 
+    }*/
+    if(!getInput(28))
+    {
+      NRF_TIMER3->TASKS_COUNT = 0x01;
     }
     printf("Timer 4 readings are: %d\t  Counter 3 readings are: %d\n", read_timer(), read_counter());
     

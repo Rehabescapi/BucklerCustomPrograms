3bfa39c9ed4ce1fc5db79ce7c8bc3216446926c5
Jason Lehmann <Jasonmlehmann@gmail.com>
2023-03-07 13:28:09 -0800

    merged into main

--- software/apps/pwm_gen/main.c
+++ software/apps/pwm_gen/main.c
@@ -55,27 +55,40 @@
 #include "app_error.h"
 #include "bsp.h"
 #include "nrf_delay.h"
-#include "app_pwm.h"\
+#include "app_pwm.h"
 
 // Create the instance "PWM1" using TIMER1.
+APP_PWM_INSTANCE(PWM1,1); 
+static volatile bool ready_flag; 
 
- 
+ void pwm_ready_callback(uint32_t pwm_id)    // PWM callback function
+{
+    ready_flag = true;
+}
+
 
 int main(void)
 {
     ret_code_t err_code;
+    
 
     /* 2-channel PWM, 200Hz, output on Buckler LED pins. */
+    app_pwm_config_t pwm_config =  APP_PWM_DEFAULT_CONFIG_2CH(5000, 23,25)
+
+    pwm_config.pin_polarity[1] = APP_PWM_POLARITY_ACTIVE_HIGH;
+
     
 
     /* Switch the polarity of the second channel. */
     
 
     /* Initialize PWM. */
-    err_code = ...
+
+
+    err_code = app_pwm_init(&PWM1,&config_pwm , p_ready_callback);
     APP_ERROR_CHECK(err_code);
     /* Enable PWM. */
-
+    app_pwm_enable(&PWM1);
     uint32_t value;
     while (true)
     {

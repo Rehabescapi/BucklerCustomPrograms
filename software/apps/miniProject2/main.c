fdde181050e58078029e5dafbb42c2470535d4d6
Jason Lehmann <Jasonmlehmann@gmail.com>
2023-03-08 12:46:27 -0800

    Lab stopping point and Project Progress

--- software/apps/miniProject2/main.c
+++ software/apps/miniProject2/main.c
@@ -18,16 +18,110 @@
 #include "nrf_pwr_mgmt.h"
 #include "nrf_serial.h"
 
+#include "buckler.h"
 
 //bookKeeping files
 #include "pwmManager.h"
 
 // Create the instance "PWM1" using TIMER1.
+static int FourSecondCount = 125000;
+static int tX = 1, tY= 2;
+
+uint32_t read_counter(){
+    
+
+  NRF_TIMER3->TASKS_CAPTURE[1] = 0x01;
+  printf("%d count" , NRF_TIMER3->CC[1]);
+  return NRF_TIMER3->CC[1];
+
+  // fill in this function for reading the timer value on calling this function
+}
+
+void TIMER4_IRQHandler(void){
+  NRF_TIMER4->EVENTS_COMPARE[tY]= 0;
+  NRF_TIMER4->TASKS_CLEAR = 0x01;
+  NRF_TIMER4->TASKS_START = 0x01;
+  
+  NRF_TIMER3->TASKS_COUNT = 0x01;
+  printf("3 Seconds and triggered an interupt!!! %d\n", read_counter());  
+}
+
+
+
+void timer_init(){
+  // fill in this function to initialize a timer of your choice
+  
+  NRF_TIMER4->TASKS_STOP = 1;
+  NRF_TIMER4->TASKS_CLEAR =1;
+
+  NRF_TIMER4->PRESCALER = 0x09;
+  NRF_TIMER4->BITMODE = 0x02;
+  NRF_TIMER4->MODE = 0x00;
+  NRF_TIMER4->INTENSET =0x40000;//setup for 
+   printf("%d INTENSET should be %d  \t\n", NRF_TIMER4->INTENSET, 0x40);
+   NRF_TIMER4->SHORTS =0x01;
+
+  NRF_TIMER4->CC[tY] = 0x1E848;//Actual thing that is stopping x
+  //printf("%d Setup  CC[2]\t %d\n", NRF_TIMER4->CC[tY], NRF_TIMER4->EVENTS_COMPARE[tY]);
+  
+ 
+ 
+  NVIC_EnableIRQ(TIMER4_IRQn);// when CC[ty] is reached flag interrupt
+  NVIC_SetPriority(TIMER4_IRQn, 0);
+
+
+
+  NRF_TIMER4->TASKS_CLEAR = 0x01;
+  NRF_TIMER4->TASKS_START = 0x01;
+
+
+  //Count every 4 seconds
+  NRF_TIMER3 -> BITMODE = 0x02;
+  NRF_TIMER3->MODE = 0x01;
+  NRF_TIMER3->TASKS_START = 0x01;
+  
+  // 93750
+  
+  
+
+}
+
+uint32_t read_timer(){
+
+
+  NRF_TIMER4->TASKS_CAPTURE[1] = 0x01;
+  return NRF_TIMER4->CC[1];
+
+    // fill in this function for reading the timer value on calling this function
+}
+
+//Functions intention is to trip when Pin# reaches a high level.
+void LABHandler(void){
+  
+  printf("Starting conditions \nConfig[0] is %p\n And INTENSET = %p",NRF_GPIOTE->CONFIG[0], NRF_GPIOTE->INTENSET);
+  
+
+  // 0x00021C01 or 138241
+  NRF_GPIOTE->CONFIG[0]=0x00021C01;//Event, Button0, onHiToLow
+
+  // 
+  NRF_GPIOTE->INTENSET = 1;
+  NRF_GPIOTE->INTENSET = 1 <<1;
+
+  NRF_GPIOTE->CONFIG[1] = 0x00031601;// Event Mode, Switch, onToggle
+  
+
+  NVIC_EnableIRQ(GPIOTE_IRQn);
+  printf("Lab_Handled\n\n");
+}
+
+
 
 
 int main(void)
 {
     pwm_init();
+    timer_init();
 
     //while(app_pwm_channel_duty_set(&PWM1, 1, 20) == NRF_ERROR_BUSY);
     
@@ -45,7 +139,7 @@ int main(void)
             setTwoChannel( value);
             
             // for catching any errors returned
-             nrf_delay_ms(50);
+             nrf_delay_ms(100);
         }
 
     }
--- software/apps/miniProject2/pwmManager.c
+++ software/apps/miniProject2/pwmManager.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 
 #include "app_error.h"
-#include "bsp.h"
+
 #include "nrf_delay.h"
 #include "app_pwm.h"
 
@@ -10,6 +10,10 @@
 static volatile bool ready_flag; 
 static ret_code_t err_code;
 APP_PWM_INSTANCE(PWM1,1);
+
+
+
+APP_PWM_INSTANCE(PWM2, 0);
 
 
 
@@ -31,7 +35,7 @@ void pwm_init()
     /* 2-channel PWM, 200Hz, output on Buckler LED pins. */
 
     app_pwm_config_t pwm_config =  APP_PWM_DEFAULT_CONFIG_2CH(5000, 23,25);
-    //app_pwm_config_t pwm_config =  APP_PWM_DEFAULT_CONFIG_1CH(5000, 22);
+    app_pwm_config_t pwm_config2 =  APP_PWM_DEFAULT_CONFIG_1CH(5000, 18);
 
     pwm_config.pin_polarity[1] = APP_PWM_POLARITY_ACTIVE_HIGH;
     pwm_config.pin_polarity[0] = APP_PWM_POLARITY_ACTIVE_LOW;
@@ -41,11 +45,13 @@ void pwm_init()
     /* Switch the polarity of the second channel. */
     //app_pwm_channel_duty_ticks_set(&PWM1,1 ,10);
 
-
+    err_code = app_pwm_init(&PWM2, &pwm_config2, pwm_ready_callback);
+    APP_ERROR_CHECK(err_code);
     err_code = app_pwm_init(&PWM1,&pwm_config , pwm_ready_callback);
     APP_ERROR_CHECK(err_code);
 
     app_pwm_enable(&PWM1);
+    app_pwm_enable(&PWM2);
     while(app_pwm_channel_duty_set(&PWM1,1,50)== NRF_ERROR_BUSY);
 
 }
@@ -56,5 +62,6 @@ void setTwoChannel( int value)
     while(app_pwm_channel_duty_set(&PWM1, 1, value) == NRF_ERROR_BUSY);
             
     while(app_pwm_channel_duty_set(&PWM1, 0, value) == NRF_ERROR_BUSY);
+    printf("%d\t", value);
          
 }

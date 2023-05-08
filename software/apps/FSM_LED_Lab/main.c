6a149f8fec94aa96b0f820c68c637ba3329465ed
Jason Lehmann <Jasonmlehmann@gmail.com>
2023-05-07 20:09:43 -0700

    Added things

--- software/apps/FSM_LED_Lab/main.c
+++ software/apps/FSM_LED_Lab/main.c
@@ -28,10 +28,9 @@
 #include "gpio.h"
 #include "states.h"
 
-void* diplayboard( void * arg){
+ 
 
-}
-
+#include <pthread.h>
 // intialize statechart variables
   // if needed
 #include "lsm9ds1.h"
@@ -40,10 +39,86 @@ NRF_TWI_MNGR_DEF(twi_mngr_instance, 5, 0);
 
 state current_state;
 
+
 void print_state(){
 
   printf("CURRENT STATE: %d \n", current_state);
 }
+
+
+
+void* displayBoard( void * arg){
+  ret_code_t error_code = NRF_SUCCESS;
+    nrf_drv_spi_t spi_instance = NRF_DRV_SPI_INSTANCE(1);
+    nrf_drv_spi_config_t spi_config = {
+    .sck_pin = BUCKLER_LCD_SCLK,
+    .mosi_pin = BUCKLER_LCD_MOSI,
+    .miso_pin = BUCKLER_LCD_MISO,
+    .ss_pin = BUCKLER_LCD_CS,
+    .irq_priority = NRFX_SPI_DEFAULT_CONFIG_IRQ_PRIORITY,
+    .orc = 0,
+    .frequency = NRF_DRV_SPI_FREQ_4M,
+    .mode = NRF_DRV_SPI_MODE_2,
+    .bit_order = NRF_DRV_SPI_BIT_ORDER_MSB_FIRST
+  };
+  error_code = nrf_drv_spi_init(&spi_instance, &spi_config, NULL, NULL);
+  APP_ERROR_CHECK(error_code);
+  // initialize display driver
+  printf("Display initialized\n");
+  display_init(&spi_instance);
+    
+
+   bool transition = false;
+  float ax =0, ay =0, az = 0;
+  unsigned int i = 0;
+
+  
+  char buf[16] = {0};
+
+
+
+
+  // loop forever, running state machine
+  while (1) {   
+     lsm9ds1_measurement_t acc_measurement = lsm9ds1_read_accelerometer();
+    // delay before continuing
+    // Note: removing this delay will make responses quicker, but will result
+    //  in printf's in this loop breaking JTAG
+    nrf_delay_ms(250);
+   
+    print_state();
+
+    
+
+    // iterate statechart
+    switch(current_state){
+      case ON: 
+        gpio_clear(BUCKLER_LED0);
+        ax = acc_measurement.x_axis;
+        ay = acc_measurement.y_axis;
+        az = acc_measurement.z_axis;
+       
+        snprintf(buf, 16, "X:%.3f  Y:%.3f", ax, az);
+        display_write(buf, 0);
+        i += 7;
+        
+        snprintf(buf, 16, "Z:%.4f", az);
+        display_write(buf, 1);
+
+        break;
+      case OFF:
+        
+        display_write("",0);
+        display_write("",1);
+        break;
+}}
+
+  
+}
+
+
+
+
 void initialize_hardware(){
   ret_code_t error_code = NRF_SUCCESS;
 
@@ -93,95 +168,44 @@ int main(void) {
   printf("Log initialized!\n");
   initialize_hardware();
 
-  // initialize LEDs
-  nrf_drv_spi_t spi_instance = NRF_DRV_SPI_INSTANCE(1);
-  
-    nrf_drv_spi_config_t spi_config = {
-    .sck_pin = BUCKLER_LCD_SCLK,
-    .mosi_pin = BUCKLER_LCD_MOSI,
-    .miso_pin = BUCKLER_LCD_MISO,
-    .ss_pin = BUCKLER_LCD_CS,
-    .irq_priority = NRFX_SPI_DEFAULT_CONFIG_IRQ_PRIORITY,
-    .orc = 0,
-    .frequency = NRF_DRV_SPI_FREQ_4M,
-    .mode = NRF_DRV_SPI_MODE_2,
-    .bit_order = NRF_DRV_SPI_BIT_ORDER_MSB_FIRST
-  };
-  error_code = nrf_drv_spi_init(&spi_instance, &spi_config, NULL, NULL);
-  APP_ERROR_CHECK(error_code);
+  pthread_t newthread;
 
-  // initialize display driver
 
-  
-  printf("Display initialized\n");
-  
+  void * exitStatus;
 
-  display_init(&spi_instance);
+  pthread_create(&newthread, NULL, displayBoard, NULL);
+
+  pthread_join(newthread, &exitStatus);
+
   
   // initialize the state variable
 
   current_state = INIT; 
-  bool transition = false;
-  float ax =0, ay =0, az = 0;
-  unsigned int i = 0;
-
-  
-  char buf[16] = {0};
+ 
   
 
   // loop forever, running state machine
   while (1) {
-    
-    
-    
-     lsm9ds1_measurement_t acc_measurement = lsm9ds1_read_accelerometer();
     // delay before continuing
     // Note: removing this delay will make responses quicker, but will result
     //  in printf's in this loop breaking JTAG
     nrf_delay_ms(250);
    
     print_state();
-
-    
-
     // iterate statechart
-    switch(current_state){
-
-      
+    switch(current_state){    
       case INIT:
         // move on to testing the button status
         current_state = OFF;
         break;
       case ON:
         
-        gpio_clear(BUCKLER_LED0);
-
-        
-        
-        
-
-        ax = acc_measurement.x_axis;
-        ay = acc_measurement.y_axis;
-        az = acc_measurement.z_axis;
-       
-        snprintf(buf, 16, "X:%.3f  Y:%.3f", ax, az);
-       
-
-        
-       
-        display_write(buf, 0);
-        i += 7;
-        
-        snprintf(buf, 16, "Z:%.4f", az);
-        display_write(buf, 1);
-
-        
+        gpio_clear(BUCKLER_LED0);    
         if (gpio_read(BUCKLER_BUTTON0) )
           {
             
             current_state = OFF;
-            display_write("",0);
-            display_write("",1);
+            
             
           }
         break;
